5dfcd5db983e2f7178fe29b66344f248f75dc9f3
Marcos Barbosa <112419190+Mesbi@users.noreply.github.com>
2025-06-16 12:46:10 -0300

    Update Lox.java

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -6,6 +6,20 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Lox {
+    public static void main(String[] args) throws IOException {
+
+        // ---- Bloco de teste temporário ----
+        Expr expression = new Expr.Binary(
+            new Expr.Unary(
+                new Token(TokenType.MINUS, "-", null, 1),
+                new Expr.Literal(123)),
+            new Token(TokenType.STAR, "*", null, 1),
+            new Expr.Grouping(
+                new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
+        // ---- Fim do bloco de teste ----
+        
     static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
