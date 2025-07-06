4e6efd91992eb4600c4d06dcb83457966af57e2e
Marcos Barbosa <112419190+Mesbi@users.noreply.github.com>
2025-07-05 21:24:17 -0300

    Update GenerateAst.java

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -11,15 +11,15 @@ public class GenerateAst {
         if (args.length != 1) {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
-            String outputDir = args[0];
         }
         String outputDir = args[0];
+        
         defineAst(outputDir, "Expr", Arrays.asList(
             "Assign   : Token name, Expr value",
             "Binary   : Expr left, Token operator, Expr right",
             "Grouping : Expr expression",
             "Literal  : Object value",
-            "Unary    : Token operator, Expr right"
+            "Unary    : Token operator, Expr right",
             "Variable : Token name"
         ));
           // NOVA chamada para gerar Stmt.java
